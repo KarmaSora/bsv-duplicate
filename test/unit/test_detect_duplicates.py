11200c20861cc8c41c1f0362c4d8d99f01ece468
KarmaSora <karammtr@gmail.com>
2025-08-28 11:09:15 +0200

    test

--- test/unit/test_detect_duplicates.py
+++ test/unit/test_detect_duplicates.py
@@ -37,32 +37,7 @@ def test_detect_duplicatesOneSingleEnrty():
         assert result == []
 
 
-@pytest.mark.unit
-def test_detect_duplicatesTwoUnique():
-        bibtex_data = """
-        @article{frattini2023requirements,
-            title={Requirements quality research: a harmonized theory, evaluation, and roadmap},
-            author={Frattini, Julian and Montgomery, Lloyd and Fischbach, Jannik and Mendez, Daniel and Fucci, Davide and Unterkalmsteiner, Michael},
-            journal={Requirements Engineering},
-            pages={1--14},
-            year={2023},
-            publisher={Springer},
-            doi={10.1007/s00766-023-00405-y}
-        }
 
-        @article{karam,
-            title={Requirements quality research: a harmonized theory, evaluation, and roadmap},
-            author={Frattini, Julian and Montgomery, Lloyd and Fischbach, Jannik and Mendez, Daniel and Fucci, Davide and Unterkalmsteiner, Michael},
-            journal={Requirements Engineering},
-            pages={1--14},
-            year={2023},
-            publisher={Springer},
-            doi={10.1047/s00766-023-00205-y}
-        }
-        """
-        result = detect_duplicates(bibtex_data)
-        
-        assert len(result) == 1
 
 
 @pytest.mark.unit
