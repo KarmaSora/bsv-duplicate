42c0f6bacf974546279b825d1a5ae86b17207da6
KarmaSora <karammtr@gmail.com>
2025-08-28 10:46:15 +0200

    test

--- test/unit/test_detect_duplicates.py
+++ test/unit/test_detect_duplicates.py
@@ -1,7 +1,19 @@
 import pytest
+from src.util.detector import detect_duplicates
+
 
 # develop your test cases here
 
 @pytest.mark.unit
 def test_detect_duplicates():
     assert True
+
+
+
+@pytest.mark.unit
+def test_detect_duplicates1():
+    bibtex_data = ""
+    result = detect_duplicates(bibtex_data)
+    assert result == []
+
+
