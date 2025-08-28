798d5c607f03ecad577d3f0f0c484c03109b69f0
KarmaSora <karammtr@gmail.com>
2025-08-28 10:54:49 +0200

    testing article enrties

--- test/unit/test_detect_duplicates.py
+++ test/unit/test_detect_duplicates.py
@@ -11,10 +11,39 @@ def test_detect_duplicates():
 
 
 @pytest.mark.unit
-def test_detect_duplicatesLessThan1():
+def test_detect_duplicatesNoEntries():
     bibtex_data = ""
     with pytest.raises(ValueError):
         detect_duplicates(bibtex_data)
 
+
+
+@pytest.mark.unit
+def test_detect_duplicatesOneSingleEnrty():
+        bibtex_data = """
+        @article{key1,
+            author = {Author One},
+            title = {Title One}
+        }
+        """
+        result = detect_duplicates(bibtex_data)
+        assert result == []
+
+
+
+@pytest.mark.unit
+def test_detect_duplicatesTwoIdenticalEntries():
+        bibtex_data = """
+        @article{key1,
+            author = {Author One},
+            title = {Title One}
+        }
+            @article{key1,
+            author = {Author One},
+            title = {Title One}
+        }
+        """
+        result = detect_duplicates(bibtex_data)
+        assert len(result) == 1
 
 
